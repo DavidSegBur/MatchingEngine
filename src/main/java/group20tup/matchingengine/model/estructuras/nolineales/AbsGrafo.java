1821d8e7445883e24a2c3155d3823b7a4c02e6c7
Ivan Gomez <noob.programer.java@gmail.com>
2026-05-27 16:31:21 -0300

    "Add comments for JavaDoc and minor improvements"

--- src/main/java/group20tup/matchingengine/model/estructuras/nolineales/AbsGrafo.java
+++ src/main/java/group20tup/matchingengine/model/estructuras/nolineales/AbsGrafo.java
@@ -5,32 +5,69 @@ import group20tup.matchingengine.model.estructuras.lineales.ListaDoubleLinkedL;
 import group20tup.matchingengine.model.estructuras.lineales.MatrizGrafo;
 import group20tup.matchingengine.model.recursos.OperacionesG;
 
+/**
+ * Clase abstracta que representa un grafo dirigido con matriz de adyacencia.
+ * <p>
+ *     Proporciona la estructura base para la representacion del grafo
+ *     mediante una matriz de costos (MatrizGrafo), e implementa los
+ *     recorridos en amplitud (BEA) y profundidad (BPF) definidos en
+ *     la interfaz OperacionesG. Las subclases concretas deben
+ *     implementar el metodo {@code cargarGrafo()} segun su origen
+ *     de datos especifico.
+ * </p>
+ * @author Catedra de AyED e Ivan
+ * @version 2.0
+ */
 public abstract class AbsGrafo implements OperacionesG {
 	protected MatrizGrafo matrizCosto;
 	protected int ordenGrafo;
 
-	public void setOrdenGrafo(int ordenGrafo) {
-		this.ordenGrafo = ordenGrafo;
-	}
-
-	protected static double infinito= Double.POSITIVE_INFINITY;
-
+	/**
+	 * Construye un grafo vacio con el orden dado.
+	 * @param ordenGrafo Cantidad de nodos del grafo
+	 */
 	public AbsGrafo(int ordenGrafo) {
 		this.ordenGrafo = ordenGrafo;
 		this.matrizCosto = new MatrizGrafo(getOrden());
 	}
 
+	/**
+	 * Establece el orden (cantidad de nodos) del grafo.
+	 * @param ordenGrafo Nuevo orden del grafo
+	 */
+	public void setOrdenGrafo(int ordenGrafo) {
+		this.ordenGrafo = ordenGrafo;
+	}
+
+	/**
+	 * Devuelve el orden (cantidad de nodos) del grafo.
+	 * @return Cantidad de nodos
+	 */
 	public int getOrden(){
 		return this.ordenGrafo;
 	}
 
+	/**
+	 * Valor infinito usado para representar ausencia de conexion.
+	 */
+	protected static double infinito= Double.POSITIVE_INFINITY;
+
+	/**
+	 * Metodo abstracto para cargar los datos del grafo.
+	 * Cada subclase debe implementar su propia logica de carga.
+	 */
 	public abstract void cargarGrafo();
+
+	/**
+	 * Muestra por consola las aristas del grafo con sus costos.
+	 * Solo muestra las conexiones existentes (costo != infinito).
+	 */
 	public void muestraGrafo(){
 		double currCost;
 		for (int i=0; i<getOrden();i++){
 			for (int j=0; j<getOrden();j++){
 				if (i!=j){
-					currCost=(double)this.matrizCosto.devolver(i, j);
+					currCost= this.matrizCosto.devolver(i, j);
 					if (currCost!=infinito){
 						System.out.println("costo " + i + " a " + j + "->" + currCost);
 					}
@@ -39,6 +76,11 @@ public abstract class AbsGrafo implements OperacionesG {
 		}
 	}
 
+	/**
+	 * Recorrido en profundidad (DFS) desde un nodo dado.
+	 * @param listaMarca Lista de booleanos que indica nodos visitados
+	 * @param v Nodo desde el cual iniciar el recorrido
+	 */
 	private void bpf(ListaDoubleLinkedL listaMarca, int v){
 		boolean marcado;
 		double currCost;
@@ -47,13 +89,16 @@ public abstract class AbsGrafo implements OperacionesG {
 		System.out.println("vertice "+ v);
 		for (int w=0;w<getOrden();w++){
 			marcado=(boolean)listaMarca.devolver(w);
-			currCost=(double)this.matrizCosto.devolver(v,w);
+			currCost=this.matrizCosto.devolver(v,w);
 			if (currCost!=infinito && !marcado){
 				bpf(listaMarca,w);
 			}
 		}
 	}
 
+	/**
+	 * Muestra el recorrido en profundidad (DFS) de todo el grafo.
+	 */
 	public void muestraBPF(){
 		ListaDoubleLinkedL listaMarca;
 		boolean marcado;
@@ -66,11 +111,16 @@ public abstract class AbsGrafo implements OperacionesG {
 		for (int v=0;v<getOrden();v++){
 			marcado=(boolean)listaMarca.devolver(v);
 			if (!marcado){
-				bea(listaMarca,v);
+				bpf(listaMarca,v);
 			}
 		}		
 	}
 
+	/**
+	 * Recorrido en amplitud (BFS) desde un nodo dado.
+	 * @param listaMarca Lista de booleanos que indica nodos visitados
+	 * @param v Nodo desde el cual iniciar el recorrido
+	 */
 	private void bea(ListaDoubleLinkedL listaMarca, int v){
 		boolean marcado;
 		double currCost;
@@ -86,7 +136,7 @@ public abstract class AbsGrafo implements OperacionesG {
 			w=(int)cola.sacar();
 			for (int z=0;z<getOrden();z++){
 				marcado=(boolean)listaMarca.devolver(z);
-				currCost=(double)this.matrizCosto.devolver(w,z);
+				currCost=this.matrizCosto.devolver(w,z);
 				if (currCost!=infinito && !marcado){
 					listaMarca.reemplazar(true, z);
 					cola.meter(z);
@@ -96,6 +146,9 @@ public abstract class AbsGrafo implements OperacionesG {
 		}
 	}
 
+	/**
+	 * Muestra el recorrido en amplitud (BFS) de todo el grafo.
+	 */
 	public void muestraBEA(){
 		ListaDoubleLinkedL listaMarca;
 		boolean marcado;
@@ -113,6 +166,10 @@ public abstract class AbsGrafo implements OperacionesG {
 		}		
 	}
 
+	/**
+	 * Devuelve la matriz de costos del grafo.
+	 * @return MatrizGrafo con los costos (ETAs) entre nodos
+	 */
 	public MatrizGrafo getMatrizCosto() {
 		return matrizCosto;
 	}
--- src/main/java/group20tup/matchingengine/model/estructuras/nolineales/GrafoDirigido.java
+++ src/main/java/group20tup/matchingengine/model/estructuras/nolineales/GrafoDirigido.java
@@ -2,12 +2,39 @@ package group20tup.matchingengine.model.estructuras.nolineales;
 
 import java.util.Scanner;
 
+/**
+ * Implementacion concreta de un grafo dirigido con matriz de adyacencia.
+ * <p>
+ *     Extiende la clase abstracta {@code AbsGrafo} y proporciona una
+ *     implementacion del metodo {@code cargarGrafo()} que permite la
+ *     carga manual de costos mediante entrada estandar (consola).
+ *     Para uso en produccion, los datos se cargan a traves de
+ *     {@code CargadorDatos.cargarMatrizVial()}.
+ * </p>
+ * @author Ivan
+ * @version 1.0
+ */
 public class GrafoDirigido extends AbsGrafo{
+
+	/**
+	 * Construye un grafo dirigido con el orden dado.
+	 * @param ordenGrafo Cantidad de nodos del grafo
+	 */
 	public GrafoDirigido(int ordenGrafo){
 		super(ordenGrafo);
 	}
 	
+	/**
+	 * Carga manual interactiva del grafo mediante entrada por consola.
+	 * <p>
+	 *     Solicita al usuario el costo de cada arista entre pares de nodos.
+	 *     Un valor de -1 indica ausencia de conexion.
+	 * </p>
+	 * @deprecated Metodo interactivo solo para propositos academicos.
+	 *             En produccion utilizar {@code CargadorDatos.cargarMatrizVial()}.
+	 */
 	@Override
+	@Deprecated
 	public void cargarGrafo(){
 		double currCost;		
 		Scanner scanner = new Scanner(System.in);

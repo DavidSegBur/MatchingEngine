746e924033824041193864ba689a4314d99239ce
David Segovia <david.segbur@gmail.com>
2026-06-03 17:09:29 -0300

    Fix mostrar imágenes Lineas 9-10

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module group20tup.matchingengine {
     opens group20tup.matchingengine.controller to javafx.fxml;
     opens group20tup.matchingengine.css to javafx.graphics;
     exports group20tup.matchingengine;
+
+    opens group20tup.matchingengine to javafx.fxml;
+    opens group20tup.matchingengine.images to javafx.graphics;
 }
